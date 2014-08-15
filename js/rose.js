d2a5e16bec7b21802582b599a4e7e47fbce4f2a6
Murat Knecht <murat.knecht@gmx.de>
2014-08-15 17:35:38 +1200

    Messed up state after finding out that the scrollbars interfere with coordinate translations. (why\!)

--- js/rose.js
+++ js/rose.js
@@ -12,7 +12,47 @@ require(
   ['jquery', 'd3'],
   function($, d3) {
     $(function() {
+
       var $w = $(window)
+
+        var hfull = $w.height()
+        var hhalf = (hfull / 2) | 0;
+        var hquarter = (hhalf / 2) | 0;
+
+        var $interactDiv = $('<div id="interact"></div>').appendTo('body')
+        var interact = d3.select('div#interact').append('svg')
+                       .attr("id", "interact")
+                       .attr("height", "100%")
+                       .attr("width", "100%")
+        interact.append('circle')
+        .attr("class", "selector")
+        .attr("cx", hquarter) //$w.width()/2)
+        .attr("cy", hquarter)
+        .attr("r", hquarter)
+        interact.append('circle')
+        .attr("class", "selector")
+        .attr("cx", hhalf) //$w.width()/2)
+        .attr("cy", hhalf)
+        .attr("r", hquarter)
+        interact.append('circle')
+        .attr("class", "selector")
+        .attr("cx", hquarter) //$w.width()/2)
+        .attr("cy", hquarter * 3)
+        .attr("r", hquarter)
+      interact.append('rect')
+      .attr('class', 'testsvg')
+      .attr('x', 800)
+      .attr('y', 50)
+      .attr('width', 200)
+      .attr('height', 100)
+      interact.append('line')
+      .attr('class', 'testsvg')
+      .attr('x1', 0)
+      .attr('y1', 0)
+      .attr('x2', 100)
+      .attr('y2', 100)
+
+      $('body').append('<div class="testdiv"></div>')
       // var $root = $('<svg></svg>')
       //             .appendTo('body')
       //             .attr('height', $w.height())
@@ -26,41 +66,21 @@ require(
           return "translate(" + $w.width() + " " + $w.height() + ")"
         }
 
-        var $div = $('<div></div').appendTo('body')
-        $div
-        .height($w.height()/2)
-        .width("100%") //$w.width()/2)
+        var $rosediv = $('<div id="rosediv"></div').appendTo('body')
+        $rosediv
+        .height(hhalf)
+        .width("100%")
         .addClass("rose")
         .css({
-          top: ($w.height()/4) | 0,
-          left: 0 //($w.width()/4) | 0
+          top: hquarter,
+          left: hquarter
         })
-        var $svg = $(xml.documentElement).appendTo($div)
-        var originalHeight = $svg.height()
-        var svg = d3.select('svg')
-                  .attr("height", "100%")
-                  .attr("width", "100%")
+        var $svg = $(xml.documentElement).appendTo($rosediv)
+        var roseSvg = d3.select('#rosediv').select('svg')
+                  .attr("height", hhalf)
+                  .attr("width", hhalf)
                   .attr("viewBox", "0 0 800 800")
                   .attr("preserveAspectRatio", "xMidYMid meet")
-                  // .attr('height', $w.height())
-                  // .attr('width', $w.width())
-        // svg.select('g')
-        //           .transition().duration(0).delay(0)
-        // .attr("transform", translateRose(svg) + " " + scaleRose(originalHeight))
-
-        var interactDiv = $('<div id="interact"></div>').appendTo('body')
-        var interact = d3.select('div#interact').append('svg')
-                       .attr("id", "interact")
-                       .attr("height", "100%")
-                       .attr("width", "100%")
-                       .attr("viewBox", "0 0 " + ($w.width() | 0) + " " + ($w.height() | 0))
-                       .attr("preserveAspectRatio", "xMidYMid meet")
-        interact.append('circle')
-        .attr("class", "selector")
-        .attr("cx", $w.width()/2)
-        .attr("cy", $w.height()/2)
-        .attr("r", $w.height()/4 + "px")
-
       })
     })
   }
